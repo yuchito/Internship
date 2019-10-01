6677eaac6520ad83f7e96d908db82a8b0d7eef9f
HoussamBenhadine <houssambenhadine21@gmail.com>
2019-10-01 21:47:59 +0200

    modify messages route - constructing JSON object

--- routes/messages.js
+++ routes/messages.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 var parseString = require('xml2js').parseString;
 var request = require('request');
+var axios = require('axios');
+const config = require('../config');
+var btoa = require('btoa');
+var xpath = require('xpath'),
+    dom = require('xmldom').DOMParser;
+
+
+
+
 var ExternalSystem = require('../models/externalSystem');
+var getMessagesFromXML = require('../modules/getMessagesFromXml');
+var Channel = require('../models/channel');
 
 
 
@@ -15,11 +26,46 @@ router.get('/', async(req, res, next)=>{
     let resp = {};
     systems.forEach(elem => {
         resp[elem.name.toLowerCase()] = {};
-        resp[elem.name.toLowerCase()]["last_message_received"] = {};
-        resp[elem.name.toLowerCase()]["last_message_sent"] = {};
+        resp[elem.name.toLowerCase()]["last_message_received"] = "";
+        resp[elem.name.toLowerCase()]["last_message_sent"] = "";
+
+
     });
+
+    let channels = await Channel.findAll();
+    let data = {};
+    channels.forEach(elem => {
+        // var doc = new dom().parseFromString('' + elem);
+        urlsMessagesPerChannel = [];
+        url = config.mirth.url + 'channels/' + elem.id + '/messages';
+        console.log(url);
+        urlsMessagesPerChannel.push(url)
+    });
+
+    await Promise.all(urlsMessagesPerChannel.map(async url => {
+        try {
+            response = await axios.get(url + '?includeContent=true&offset=0&limit=20', {
+            proxy: {
+                host: '10.23.201.11',
+                port: 3128,
+            },
+            headers: { Authorization: 'Basic ' + btoa(config.mirth.user + ':' + config.mirth.password) }
+        });
+    } catch (err) {
+        res.json(err)
+    }
+        
+    
+    })); 
     console.log(resp);
-    res.json(resp);
+return res.json(resp);
+
+
+
+
+    // console.log(resp);
+    // res.json(resp);
+    
     // proxy: 'http://10.23.201.11:3128',
 //     headers : {
 //         responseType:'application/xml',
