d7f7c42360ab28567758813b4d665531e412aecc
HoussamBenhadine <houssambenhadine21@gmail.com>
2019-10-01 19:19:45 +0200

    modifying message model

--- models/message.js
+++ models/message.js
@@ -1,13 +1,16 @@
-class Message {
-    constructor(type, sent, received, date, externalsystem, facility, ackNack) {
-        this.type = type;
-        this.sent = sent;
-        this.date = date;
-        this.externalsystem = externalsystem;
-        this.facility = facility;
-        this.received = received;
-        this.ackNack = ackNack
-    }
-}
+const Sequelize = require('sequelize'),
 
-module.exports = Message;
+const db = require('../config/database'),
+
+const Message = db.define('externalsystems', {
+    type : type,
+      isReceived : isReceived,
+      date : date,
+      externalsystem : externalsystem,
+      facility : facility,
+      ackNack : ackNack
+});
+
+
+
+module.exports = Message
