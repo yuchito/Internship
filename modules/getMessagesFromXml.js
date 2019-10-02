f9717ea771e68cc40a336aa1ba5ba975bbe6a247
HoussamBenhadine <houssambenhadine21@gmail.com>
2019-10-02 11:52:02 +0200

    Implement generic function to retreive content of messages for each channel

--- modules/getMessagesFromXml.js
+++ modules/getMessagesFromXml.js
@@ -1,11 +1,28 @@
-const formatMessage = require('./formatMessage');
+const formatReceivedMessage = require('./formatReceivedMessage');
+const formatSentMessage = require('./formatSentMessage');
+
+var xpath = require('xpath')
+  , dom = require('xmldom').DOMParser
+
 
 getMessagesFromXML = function(str) {
-    messages = [];
+    var  _messages = [];
     // str parsing
+    var doc = new dom().parseFromString(str);
+    var messages = xpath.select("/list/message/connectorMessages", doc);
+    messages.forEach(msg => {
+        var doc = new dom().parseFromString('' + msg);
+        var entryReceived = xpath.select("//entry[int='0']", doc);
+        if (formatReceivedMessage(entryReceived)) {
+            _messages.push(formatReceivedMessage(entryReceived));
+        }
+        var entrySent = xpath.select("//entry[int!='0' and last()]", doc);
+        // if (formatSentMessage(entrySent)) {
+        //     _messages.push(formatSentMessage(entrySent));
+        // }
+        // var sents = xpath.select("//entry[int!='0']", doc);
+    });
 
-
-
-    messages.push(formatMessage(str));
+    return _messages;
 }
 module.exports = getMessagesFromXML;
